1e81c400708f0edeb914c1f585ed081466c6e456
Ahmed Sarkawt <40734891+Ahmed-Sarkawt@users.noreply.github.com>
2021-07-02 08:49:28 +0300

    Update winning items

--- src/data/items.ts
+++ src/data/items.ts
@@ -2,13 +2,19 @@ import { IWinnable, Winnable, WinnableType } from "@/models/winnable";
 import Item from "@/models/item";
 
 export const items: IWinnable[] = [
-  new Winnable(WinnableType.item, 0.18, new Item("5$ Arabela Store Credit")),
-  new Winnable(WinnableType.item, 0.12, new Item("10$ Arabela Store Credit")),
+  
+  new Winnable(WinnableType.item, 0.2, new Item("5$ Arabela Store Credit")),
+  new Winnable(WinnableType.item, 0.1, new Item("10$ Arabela Store Credit")),
+  new Winnable(WinnableType.item, 0.1, new Item("15 Arabela Store Credit")),
+  new Winnable(WinnableType.item, 0.05, new Item("15 Arabela Store Credit")),
+  new Winnable(WinnableType.item, 0.04, new Item("25$ Arabela Store Credit")),
   new Winnable(WinnableType.item, 0.01, new Item("50$ Arabela Store Credit")),
+  
   new Winnable(WinnableType.item, 0.15, new Item("1LB Free Shipping")),
-  new Winnable(WinnableType.item, 0.1, new Item("2LB Free Shipping")),
-  new Winnable(WinnableType.item, 0.01, new Item("5LB Free Shipping")),
-  new Winnable(WinnableType.item, 0.05, new Item("PS5 Controller")),
-  new Winnable(WinnableType.tryAgain, 0.18),
-  new Winnable(WinnableType.nothing, 0.2),
+  new Winnable(WinnableType.item, 0.04, new Item("2B Free Shipping")),
+  new Winnable(WinnableType.item, 0.01, new Item("3B Free Shipping")),
+  
+
+  new Winnable(WinnableType.tryAgain, 0.15),
+  new Winnable(WinnableType.nothing, 0.15,
 ];
